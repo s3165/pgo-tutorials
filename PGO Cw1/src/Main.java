f9e5274558d2d1b48c9960373e60852aefebd5f2
Michal Wawrylo <s3165@pjwstk.edu.pl>
2024-04-07 17:22:01 +0200

    change main

--- PGO Cw1/src/Main.java
+++ PGO Cw1/src/Main.java
@@ -1,46 +1,24 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Wprowadz dowolną liczbę:");
+        double x=scanner.nextDouble();
 
-
-        import java.util.Scanner;
-        import java.io.*;
-        class InputScanner
-        {
-            public static void main(String[] args)
-            {
-// Declarations of variables
-                Scanner scant = new Scanner(System.in);
-                int integer_type;
-                long long_type
-                float real_type;
-                double double_type;
-                String string2;
-// Prompts the User
-                System.out.println("Enter three numbers integer,long and float ");
-                System.out.println("Enter number, another floating-point number, " + "and a string.");
-                System.out.println("Separate each input with  blank or return.");
-// Read in values
-                integer_type = scant.nextInt();
-                long_type = scant.nextLong();//taking long type input
-                real_type = scant.nextFloat();//taking float type input
-                double_type = scant.nextDouble();//taking double type input
-                System.out.println("enter one more value.");
-                string2 = scant.next(); // string input
-                System.out.println("Here is the output Created: ");
-                System.out.println(integer_type + " " + long_type + " " + real_type + " " + double_type + " " + string2);
-            }
+        if(x<0){
+            System.out.println("Liczba mniejsza od zera");
         }
-        
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        else if (x==0) {
+            System.out.println("Liczba rowna zero");
         }
+        else if (x>0) {
+            System.out.println("Liczba wieksza od zera");
+        }
+
+
     }
 }
